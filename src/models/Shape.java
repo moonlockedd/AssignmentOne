cb152c6e78a5f42f8f25ae3e02ff01fd6f5726c4
Nurali <231087@astanait.edu.kz>
2023-12-23 10:32:22 +0600

    Add getLongestSide method

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -30,4 +30,20 @@ public class Shape {
         return calculatePerimeter() / this.points.size();
     }
 
+    public double getLongestSide() {
+        double longestSide = this.points.get(0).distanceTo(this.points.get(1));
+
+        for (int i = 1, size = this.points.size(); i < size; i++) {
+            double currentSide;
+
+            if (i + 1 != size)
+                currentSide = this.points.get(i).distanceTo(this.points.get(i + 1));
+            else
+                currentSide = this.points.get(i).distanceTo(this.points.getFirst());
+
+            if (currentSide > longestSide)
+                longestSide = currentSide;
+
+        }
+    }
 }

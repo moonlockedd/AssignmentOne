cc396f1b88ccfcaa77b4622c217c31d5d095206d
Nurali <231087@astanait.edu.kz>
2023-12-23 09:53:20 +0600

    Add distanceTo function to Point

--- src/models/Point.java
+++ src/models/Point.java
@@ -16,4 +16,13 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    public double distanceTo(Point nextPoint) {
+        double x1 = x;
+        double y1 = y;
+        double x2 = nextPoint.getX();
+        double y2 = nextPoint.getY();
+
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
 }

b70473851cacd54e825aee826cbd1ef1818c36d4
Nurali <231087@astanait.edu.kz>
2023-12-23 10:14:30 +0600

    Add getPerimeter method

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -13,4 +13,17 @@ public class Shape {
     public void addPoint(Point point) {
         this.points.add(point);
     }
+
+    public double getPerimeter() {
+        double perimeter = 0;
+
+        for (int i = 0; i < this.points.size(); i++) {
+            if (i + 1 != this.points.size())
+                perimeter += this.points.get(i).distanceTo(this.points.get(i + 1));
+            else
+                perimeter += this.points.get(i).distanceTo(this.points.getFirst());
+        }
+
+        return perimeter;
+    }
 }
